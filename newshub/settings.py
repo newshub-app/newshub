8d6e071e3e39441c2981b3c02c29fe5f96f02830
Mathieu Deous <mat.deous@gmail.com>
2024-06-16 16:50:24 +0200

    :lipstick: use bootstrap

--- newshub/settings.py
+++ newshub/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
+    "django_bootstrap5",
     "news.apps.NewsConfig",
     "api.apps.ApiConfig"
 ]
