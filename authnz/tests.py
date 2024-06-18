03c9973fe58ce35c878a899fdd78009e2e6075d1
Mathieu Deous <mat.deous@gmail.com>
2024-06-19 01:43:14 +0200

    :white_check_mark: authentication tests

--- authnz/tests.py
+++ authnz/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+EXAMPLE_USER = {
+    "username": "testuser",
+    "first_name": "Test",
+    "last_name": "User",
+    "password": "SecureTestPassword123!",
+    "email": "testuser@example.com",
+}
+
+
+class AuthenticationTestCase(TestCase):
+    def test_login(self):
+        response = self.client.get(reverse("login"))
+        self.assertContains(response, "Login", status_code=200)
+
+    def test_register(self):
+        response = self.client.post(reverse("register"), data=EXAMPLE_USER)
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        response = self.client.post(reverse("logout"))
+        self.assertEqual(response.status_code, 302)
