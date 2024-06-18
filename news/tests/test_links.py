23984cc4274494dfd8670cab7dcd8e520c987354
Mathieu Deous <mat.deous@gmail.com>
2024-06-18 20:28:18 +0200

    :white_check_mark: improve unit tests

--- news/tests/test_links.py
+++ news/tests/test_links.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
 
+INDEX_TITLE = "NewsHub - Collaborative news sharing"
+LINKS_TITLE = "NewsHub - Links"
+LINK_EDIT_TITLE = "NewsHub - Edit link"
+EXAMPLE_LINK_TITLE = "Test link title"
 EXAMPLE_LINK = {
     "url": "https://example.com",
-    "title": "Example",
+    "title": EXAMPLE_LINK_TITLE,
     "description": "Example description",
     "category": 1
 }
+UPDATED_TITLE = "Modified"
 
 
 class LinksTestCase(TestCase):
@@ -13,18 +18,24 @@ class LinksTestCase(TestCase):
 
     def test_index(self):
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, INDEX_TITLE, status_code=200)
 
     def test_link_create(self):
         response = self.client.post("/links/new/", data=EXAMPLE_LINK, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name, ["news/link_list.html"])
+        self.assertContains(response, EXAMPLE_LINK_TITLE, status_code=200)
 
     def test_links(self):
         response = self.client.get("/links/")
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, LINKS_TITLE, status_code=200)
 
-    def test_link_update(self):
+    def test_link_update_get(self):
         self.client.post("/links/new/", data=EXAMPLE_LINK)
-        response = self.client.post("/links/1/", data={"title": "New title"})
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get("/links/1/")
+        self.assertContains(response, LINK_EDIT_TITLE, status_code=200)
+
+    def test_link_update_post(self):
+        self.client.post("/links/new/", data=EXAMPLE_LINK)
+        modified_link = EXAMPLE_LINK.copy()
+        modified_link["title"] = UPDATED_TITLE
+        response = self.client.post("/links/1/", data=modified_link, follow=True)
+        self.assertContains(response, UPDATED_TITLE, status_code=200)
