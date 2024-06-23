5c7c1dee1bd06ad9efb91fc7a9bbbcac13e0e071
Mathieu Deous <mat.deous@gmail.com>
2024-06-23 13:29:14 +0200

    :recycle: make url label uppercase

--- news/forms.py
+++ news/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, URLField
 
 from .models import *
 
 
 class LinkForm(ModelForm):
+    url = URLField(label="URL")
+
     class Meta:
         model = Link
         fields = ["url", "title", "description", "category"]
