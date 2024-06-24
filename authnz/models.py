152313c986a4a6925a4baf5cec233b88974df03c
Mathieu Deous <mat.deous@gmail.com>
2024-06-24 02:17:28 +0200

    :sparkles: users have an api key

--- authnz/models.py
+++ authnz/models.py
@@ -1,10 +1,20 @@
+import hashlib
+from random import SystemRandom
+
 from django.contrib.auth.models import AbstractUser, Group as BaseGroup
+from django.db.models import CharField
 
 __all__ = ["User", "Group"]
 
 
+def generate_api_key():
+    rand = SystemRandom()
+    rand_bytes = rand.randbytes(40)
+    return hashlib.sha1(rand_bytes).hexdigest()
+
+
 class User(AbstractUser):
-    pass
+    api_key = CharField(max_length=40, null=False, default=generate_api_key)
 
 
 class Group(BaseGroup):
