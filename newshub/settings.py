05bd5f7e224a8539580a63b3065c105b24c5378c
Mathieu Deous <mat.deous@gmail.com>
2024-06-24 22:14:38 +0200

    :lipstick: use unfold admin panel

--- newshub/settings.py
+++ newshub/settings.py
@@ -42,6 +42,13 @@ INTERNAL_IPS = os.environ.get(
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    # "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    # "unfold.contrib.guardian",  # optional, if django-guardian package is used
+    # "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
