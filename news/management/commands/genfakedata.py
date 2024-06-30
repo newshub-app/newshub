7faf74aa06babfa7e9fe2553e78a9f5c3f74fdcd
Mathieu Deous <mat.deous@gmail.com>
2024-07-01 01:21:47 +0200

    :sparkles: fake data generation mgmt command

--- news/management/commands/genfakedata.py
+++ news/management/commands/genfakedata.py
@@ -1,4 +1,4 @@
-from random import randrange
+import random
 
 from django.core.management.base import BaseCommand
 from faker import Faker
@@ -11,8 +11,13 @@ class Command(BaseCommand):
     help = "Generate fake data for testing"
 
     def add_arguments(self, parser):
+        parser.add_argument("--users", action="store_true", help="Generate users")
         parser.add_argument(
             "--num-users", default=5, type=int, help="Number of users to generate"
+        )
+
+        parser.add_argument(
+            "--categories", action="store_true", help="Generate categories"
         )
         parser.add_argument(
             "--num-categories",
@@ -20,6 +25,8 @@ class Command(BaseCommand):
             type=int,
             help="Number of categories to generate",
         )
+
+        parser.add_argument("--links", action="store_true", help="Generate links")
         parser.add_argument(
             "--num-links", default=200, type=int, help="Number of links to generate"
         )
@@ -29,42 +36,56 @@ class Command(BaseCommand):
         num_users = options["num_users"]
         num_categories = options["num_categories"]
         num_links = options["num_links"]
-        users = []
-        categories = []
+        users = [u for u in User.objects.all()]
+        categories = [c for c in Category.objects.all()]
         links = []
 
-        for _ in range(num_users):
-            password = fake.password(length=20)
-            user = User.objects.create_user(
-                username=fake.user_name(),
-                email=fake.email(),
-                first_name=fake.first_name(),
-                last_name=fake.last_name(),
-                password=password,
-            )
-            users.append(user)
-            self.stdout.write("New user: username={user.username} password={password}")
-        self.stdout.write(self.style.SUCCESS(f"Generated {len(users)} users"))
+        if options["users"]:
+            for _ in range(num_users):
+                password = fake.password(length=20)
+                user = User.objects.create_user(
+                    username=fake.user_name(),
+                    email=fake.email(),
+                    first_name=fake.first_name(),
+                    last_name=fake.last_name(),
+                    password=password,
+                )
+                users.append(user)
+                self.stdout.write(
+                    f"New user: username={user.username} password={password}"
+                )
+            self.stdout.write(self.style.SUCCESS(f"Generated {len(users)} users"))
 
-        for _ in range(num_categories):
-            cat = Category.objects.create(
-                name=fake.word(),
+        if options["categories"]:
+            for _ in range(num_categories):
+                known_cats = Category.objects.values_list("name", flat=True)
+                cat_name = fake.word()
+                while cat_name in known_cats:
+                    cat_name = fake.word()
+                cat = Category.objects.create(
+                    name=fake.word(),
+                )
+                self.stdout.write(f"New category: {cat.name}")
+                categories.append(cat)
+            self.stdout.write(
+                self.style.SUCCESS(f"Generated {len(categories)} categories")
             )
-            categories.append(cat)
-        self.stdout.write(self.style.SUCCESS(f"Generated {len(categories)} categories"))
 
-        for _ in range(num_links):
-            url = fake.url()
-            while url in [link.url for link in links]:
+        if options["links"]:
+            for _ in range(num_links):
+                known_urls = Link.objects.values_list("url", flat=True)
                 url = fake.url()
-            link = Link.objects.create(
-                url=url,
-                title=fake.text(max_nb_chars=100),
-                description=fake.paragraph(),
-                category=categories[randrange(len(categories))],
-                created_by=users[randrange(len(users))],
-            )
-            links.append(link)
-        self.stdout.write(self.style.SUCCESS(f"Generated {len(links)} links"))
+                while url in known_urls:
+                    url = fake.url()
+                link = Link.objects.create(
+                    url=url,
+                    title=fake.text(max_nb_chars=100),
+                    description=fake.paragraph(),
+                    category=random.choice(categories),
+                    created_by=random.choice(users),
+                )
+                self.stdout.write(f"New link: {link.url}")
+                links.append(link)
+            self.stdout.write(self.style.SUCCESS(f"Generated {len(links)} links"))
 
         self.stdout.write(self.style.SUCCESS("Done generating fake data"))
