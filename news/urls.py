849518b8b3045e89505d0c0b7929d6bfa4262564
Mathieu Deous <mat.deous@gmail.com>
2024-07-05 20:30:50 +0200

    :recycle: don't cache for now

--- news/urls.py
+++ news/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import *
 
 app_name = "news"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("links/", cache_page(60 * 15)(LinkListView.as_view()), name="links"),
+    path("links/", LinkListView.as_view(), name="links"),
     path("links/<int:pk>/", LinkUpdateView.as_view(), name="link_update"),
     path("links/new/", LinkCreateView.as_view(), name="link_create"),
-    path(
-        "newsletters/",
-        cache_page(60 * 15)(NewsletterListView.as_view()),
-        name="newsletters",
-    ),
+    path("newsletters/", NewsletterListView.as_view(), name="newsletters"),
 ]
