736784c808b4520c8556c006af32a6a33dd7eddd
Mathieu Deous <mat.deous@gmail.com>
2024-07-07 13:00:53 +0200

    :lipstick: use proper admin config for unfold

--- authnz/admin.py
+++ authnz/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin
+from django.contrib.auth.models import Group as DjangoGroup
+from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import *
+
+
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+admin.site.unregister(DjangoGroup)
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Group, GroupAdmin)
--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django_celery_beat.admin import (
+    ClockedScheduleAdmin as BaseClockedScheduleAdmin,
+    CrontabScheduleAdmin as BaseCrontabScheduleAdmin,
+    PeriodicTaskAdmin as BasePeriodicTaskAdmin,
+    PeriodicTaskForm,
+    TaskSelectWidget,
+)
+from django_celery_beat.models import (
+    PeriodicTask,
+    IntervalSchedule,
+    CrontabSchedule,
+    SolarSchedule,
+    ClockedSchedule,
+)
+from unfold.admin import ModelAdmin
+from unfold.widgets import UnfoldAdminSelectWidget, UnfoldAdminTextInputWidget
 
 from .models import *
 
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(SolarSchedule)
+admin.site.unregister(ClockedSchedule)
 
-class NewsletterAdmin(admin.ModelAdmin):
+
+@admin.register(Newsletter)
+class NewsletterAdmin(ModelAdmin):
     list_display = ["date_sent"]
     search_fields = [
         "recipients__username",
@@ -17,12 +40,14 @@ class NewsletterAdmin(admin.ModelAdmin):
     date_hierarchy = "date_sent"
 
 
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
     list_display = ["name"]
     search_fields = ["name"]
 
 
-class LinkAdmin(admin.ModelAdmin):
+@admin.register(Link)
+class LinkAdmin(ModelAdmin):
     list_display = ["title", "category", "url", "created", "updated"]
     search_fields = ["title", "category__name", "url", "description"]
     list_filter = [
@@ -33,6 +58,37 @@ class LinkAdmin(admin.ModelAdmin):
     date_hierarchy = "created"
 
 
-admin.site.register(Newsletter, NewsletterAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Link, LinkAdmin)
+class UnfoldTaskSelectWidget(UnfoldAdminSelectWidget, TaskSelectWidget):
+    pass
+
+
+class UnfoldPeriodicTaskForm(PeriodicTaskForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["task"].widget = UnfoldAdminTextInputWidget()
+        self.fields["regtask"].widget = UnfoldTaskSelectWidget()
+
+
+@admin.register(PeriodicTask)
+class PeriodicTaskAdmin(BasePeriodicTaskAdmin, ModelAdmin):
+    form = UnfoldPeriodicTaskForm
+
+
+@admin.register(IntervalSchedule)
+class IntervalScheduleAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(CrontabSchedule)
+class CrontabScheduleAdmin(BaseCrontabScheduleAdmin, ModelAdmin):
+    pass
+
+
+@admin.register(SolarSchedule)
+class SolarScheduleAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(ClockedSchedule)
+class ClockedScheduleAdmin(BaseClockedScheduleAdmin, ModelAdmin):
+    pass
