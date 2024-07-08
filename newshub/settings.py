84df75e59b3d50f215b91852883ba2a94d4ff7ac
Mathieu Deous <mat.deous@gmail.com>
2024-07-08 18:42:48 +0200

    :lipstick: add link to api to user menu

--- newshub/settings.py
+++ newshub/settings.py
@@ -236,6 +236,9 @@ REST_FRAMEWORK = {
         "authnz.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "URL_FIELD_NAME": "resource_url",
