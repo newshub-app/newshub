54bce8d605696b592c727d749f9ee921c0967a72
Mathieu Deous <mat.deous@gmail.com>
2024-07-28 21:09:30 +0200

    :truck: redirect back to user profile after editing

--- authnz/views.py
+++ authnz/views.py
@@ -14,7 +14,7 @@ class ProfileView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserForm
     template_name = "authnz/profile.html"
-    success_url = reverse_lazy("news:links")
+    success_url = reverse_lazy("authnz:profile")
 
     def get_object(self, queryset=None):
         return self.request.user
