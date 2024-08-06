4f471ff8819b4828068104358106241bcc18dc29
Mathieu Deous <mat.deous@gmail.com>
2024-08-06 23:44:15 +0200

    :recycle: make routes consistent

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 app_name = "news"
 urlpatterns = [
-    path("", LinkListView.as_view(), name="links"),
+    path("links/", LinkListView.as_view(), name="links"),
     path("links/<int:pk>/", LinkUpdateView.as_view(), name="link_update"),
     path("links/new/", LinkCreateView.as_view(), name="link_create"),
     path("newsletters/", NewsletterListView.as_view(), name="newsletters"),
