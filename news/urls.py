afc936da3a6b262a4e14ed295d62b05be6fd4adb
Mathieu Deous <mat.deous@gmail.com>
2024-11-24 23:21:08 +0100

    :truck: set links list as index page

--- news/urls.py
+++ news/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 app_name = "news"
 urlpatterns = [
-    path("links/", LinkListView.as_view(), name="links"),
-    path("links/<int:pk>/", LinkUpdateView.as_view(), name="link_update"),
-    path("links/new/", LinkCreateView.as_view(), name="link_create"),
+    path("", LinkListView.as_view(), name="index"),
+    path("<int:pk>/", LinkUpdateView.as_view(), name="link_update"),
+    path("new/", LinkCreateView.as_view(), name="link_create"),
     path("newsletters/", NewsletterListView.as_view(), name="newsletters"),
     path("feeds/", FeedListView.as_view(), name="feeds"),
     path("feeds/new/", FeedCreateView.as_view(), name="feed_create"),
